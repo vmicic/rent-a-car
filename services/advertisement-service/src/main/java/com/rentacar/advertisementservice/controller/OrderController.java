5f6bdc111d8373e47dc189c1625f77f1b8e7c168
Vukasin Micic <vukasin.micic5@gmail.com>
2020-06-02 16:54:15 +0200

    Add configuration to gateway

--- services/advertisement-service/src/main/java/com/rentacar/advertisementservice/controller/OrderController.java
+++ services/advertisement-service/src/main/java/com/rentacar/advertisementservice/controller/OrderController.java
@@ -1,5 +1,6 @@
-package com.rentacar.orderservice.controller;
+package com.rentacar.advertisementservice.controller;
 
+import com.rentacar.advertisementservice.domain.dto.ReservationDTO;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -9,7 +10,6 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.rentacar.orderservice.domain.dto.ReservationDTO;
 
 @RestController
 @RequestMapping("/orders")
